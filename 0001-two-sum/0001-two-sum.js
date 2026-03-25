d2de2b52eb9fb1d326f217d86f66a66f84f513d9
snowari <70631889+snowari@users.noreply.github.com>
2026-03-25 16:07:28 +0900

    Time: 3 ms (57.63%), Space: 58.8 MB (5.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,23 +1,14 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 const twoSum = function(nums, target) {
-    const indexArr = nums.map((value, index) => ({index, value}))
-    indexArr.sort((a,b)=> a.value - b.value)
-
-    let [l, r] = [0, indexArr.length-1]
-    while(l<r) {
-        let sum = indexArr[l].value + indexArr[r].value
-        if(sum < target){
-            l++
-        }else if(sum > target){
-            r--
-        }else{
-            return [indexArr[l].index, indexArr[r].index]
+    const memo = {}
+    for(const [i, num] of nums.entries()){
+        const needed = target - num
+        if(needed in memo){
+            return [memo[needed], i]
         }
+        memo[num] = i
     }
+
+    return [-1, -1]
 
     //console.log(indexArr)
 };
