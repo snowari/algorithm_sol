a814888230645f1c9978fddfa328e4e875d959c2
snowari <70631889+snowari@users.noreply.github.com>
2026-04-20 16:18:53 +0900

    Time: 1 ms (57.8%), Space: 53.5 MB (77.38%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+
+    const grid = Array.from({ length: m }, () => Array(n).fill(1))
+    
+        for(let i=1; i<m; i++){
+            for(let j=1; j<n; j++){
+                grid[i][j] = grid[i-1][j] + grid[i][j-1]
+            }
+        }
+    
+    
+    return(grid[m-1][n-1])
+};
