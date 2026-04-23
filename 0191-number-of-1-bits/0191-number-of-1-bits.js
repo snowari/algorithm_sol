cb0f52e14be042ec455ca38bf9496c6b2d4677e7
snowari <70631889+snowari@users.noreply.github.com>
2026-04-23 17:02:10 +0900

    Time: 0 ms (100%), Space: 55.2 MB (29.11%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -3,23 +3,11 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    answer =0
-    stack = []
+    let count = 0
     while(n>0){
-        stack.push(n%2)
-        if(n/2 < 2){
-            stack.push(Math.floor(n/2))
-            break
-        }
-        n= Math.floor(n/2)
+        count += n & 1
+        n = n >>> 1
     }
 
-    console.log(stack)
-    stack.forEach(bit => {
-        if (bit === 1) {
-            answer++
-        }
-    })
-
-    return answer
+    return count
 };
