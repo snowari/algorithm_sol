642d71a9124dd3c15e42489606db20ba89c83538
snowari <70631889+snowari@users.noreply.github.com>
2026-05-15 15:22:53 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
 
 ### 성능 요약
 
-메모리: 32.9 MB, 시간: 0.29 ms
+메모리: 9.05 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 12일 00:16:23
+2026년 05월 15일 15:22:48
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def solution(n, computers):
+    graph = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(i + 1, n):
+            if computers[i][j] == 1:
+                graph[i].append(j)
+                graph[j].append(i)
+    
+    answer = 0
+    visited = [False] * n
+    
+    for i in range(n):
+        if not visited[i]:
+            answer += 1
+            queue = deque([i])
+            visited[i] = True
+            while queue:
+                curr = queue.popleft()
+                for neighbor in graph[curr]:
+                    if not visited[neighbor]:
+                        visited[neighbor] = True
+                        queue.append(neighbor)
+    
+    return answer
