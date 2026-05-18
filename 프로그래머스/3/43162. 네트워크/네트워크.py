0df1e8a01b924cef275407d07949ba26adacac6b
snowari <70631889+snowari@users.noreply.github.com>
2026-05-18 12:57:21 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,26 +1,25 @@
 from collections import deque
 
 def solution(n, computers):
+    answer =0
+    visited = [False] * n
     graph = [[] for _ in range(n)]
     for i in range(n):
-        for j in range(i + 1, n):
-            if computers[i][j] == 1:
+        for j in range(i+1, n):
+            if computers[i][j] == 1 :
                 graph[i].append(j)
                 graph[j].append(i)
     
-    answer = 0
-    visited = [False] * n
     
     for i in range(n):
         if not visited[i]:
-            answer += 1
+            answer +=1
             queue = deque([i])
             visited[i] = True
-            while queue:
+            while queue :
                 curr = queue.popleft()
                 for neighbor in graph[curr]:
-                    if not visited[neighbor]:
+                    if not visited[neighbor] :
                         visited[neighbor] = True
                         queue.append(neighbor)
-    
     return answer
